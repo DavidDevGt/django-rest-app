d5b9cc52ae9a4cf106ef0dad1e67b8d9add36ef6
DavidDevGt <josuedavidvl18@gmail.com>
2024-01-17 10:58:48 -0600

    Add status and img to Project model for api v2

--- projects/models.py
+++ projects/models.py
@@ -1,8 +1,19 @@
 from django.db import models
+from django.utils.translation import gettext_lazy as _
 
-# Create your models here.
 class Project(models.Model):
+    class Status(models.TextChoices):
+        ACTIVE = 'ACT', _('Active')
+        COMPLETED = 'COM', _('Completed')
+        PAUSED = 'PAU', _('Paused')
+
     title = models.CharField(max_length=200)
     description = models.TextField()
     tech = models.CharField(max_length=200)
+    status = models.CharField(
+        max_length=3,
+        choices=Status.choices,
+        default=Status.ACTIVE,
+    )
+    image = models.ImageField(upload_to='projects/', null=True, blank=True)
     created_at = models.DateTimeField(auto_now_add=True)
