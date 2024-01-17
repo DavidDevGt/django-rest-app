a01c2f76ccc38645c7a38ac03d52b01130559f9b
DavidDevGt <josuedavidvl18@gmail.com>
2024-01-17 12:08:54 -0600

    Add drf_yasg for API documentation

--- projects/urls.py
+++ projects/urls.py
@@ -1,8 +1,15 @@
+from django.urls import path, re_path
+from projects.views import schema_view
 from rest_framework import routers
 from .api import ProjectViewSet
 
 router = routers.DefaultRouter()
-
 router.register('api/projects', ProjectViewSet, 'projects')
 
-urlpatterns = router.urls
+urlpatterns = [
+
+    # URLs de documentación
+    re_path(r'^swagger(?P<format>\.json|\.yaml)$', schema_view.without_ui(cache_timeout=0), name='schema-json'),
+    path('swagger/', schema_view.with_ui('swagger', cache_timeout=0), name='schema-swagger-ui'),
+    path('redoc/', schema_view.with_ui('redoc', cache_timeout=0), name='schema-redoc'),
+] + router.urls
